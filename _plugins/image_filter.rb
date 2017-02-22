8e02756f38e036bfa8593d388a6e788f94684c1f
Brandon Arbini <b@arbini.com>
2017-02-21 22:12:46 -0800

    Improved image filter.

--- _plugins/image_filter.rb
+++ _plugins/image_filter.rb
@@ -3,21 +3,27 @@ module Jekyll
     def image_src(image)
       case ENV['JEKYLL_ENV']
       when "staging" then
-        %{data-src="/wayfareio-staging#{image}"}
+        %{data-src="wayfareio-staging#{image}"}
       when "production" then
-        %{data-src="/wayfareio#{image}"}
+        %{data-src="wayfareio#{image}"}
       else
         %{src="#{image}"}
       end
     end
 
     def image_class(classes)
+      if !classes.nil? && classes != ""
+        classes = classes.split(" ")
+      else
+        classes = []
+      end
       case ENV['JEKYLL_ENV']
-      when "staging" then "img-cloud #{classes}"
-      when "production" then "img-cloud #{classes}"
+      when "staging" then classes << "img-cloud"
+      when "production" then classes << "img-cloud"
       else
         classes
       end
+      classes.join(" ")
     end
   end
 end
